878413a84e5c5676fc4d887fca7855d6a846c6af
Corey Walsh <matthias61984@hotmail.com>
2018-10-08 16:03:55 -0700

    users log-in set

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,50 +1,67 @@
 // Initialize Firebase
-    var config = {
-        apiKey: "AIzaSyDvrSqX7KcnqxgNufqE3gnV1LAULzmyTrs",
-        authDomain: "homework-c1eed.firebaseapp.com",
-        databaseURL: "https://homework-c1eed.firebaseio.com",
-        projectId: "homework-c1eed",
-        storageBucket: "homework-c1eed.appspot.com",
-        messagingSenderId: "216750042287"
-    };
+var config = {
+    apiKey: "AIzaSyDvrSqX7KcnqxgNufqE3gnV1LAULzmyTrs",
+    authDomain: "homework-c1eed.firebaseapp.com",
+    databaseURL: "https://homework-c1eed.firebaseio.com",
+    projectId: "homework-c1eed",
+    storageBucket: "homework-c1eed.appspot.com",
+    messagingSenderId: "216750042287"
+};
 
-    firebase.initializeApp(config);
-    var database = firebase.database();
-    var ref = database.ref();
-    var usersRef = ref.child("users");
+firebase.initializeApp(config);
+var database = firebase.database();
+var ref = database.ref();
+var usersRef = ref.child("users");
 
-    var connectionsRef = database.ref("/connections");
-    var connectedRef = database.ref(".info/connected");
-    connectedRef.on("value", function(snap) {
-        if (snap.val()) {
-          var con = connectionsRef.push(true);
-          con.onDisconnect().remove();
-        }
-      });
-    
-    $("#nameEntry").on("click", function(event) {
-        event.preventDefault();
-        var input = $("input").val().trim();
-        connectionsRef.on("value", function(snap) {
-            if ($("#player1name").val("Donkey1") == true) {
-                usersRef.set({
-                    player1: input,
-                    wins: 0,
-                    losses: 0,
-                })
-                $("#player1name").text(input);
+var connectionsRef = database.ref("/connections");
+var connectedRef = database.ref(".info/connected");
+var userIam;
+connectedRef.on("value", function (snap) {
+    var con;
+    if (snap.val()) {
+        database.ref("/connections").once("value").then(ss => {
+            if (ss.val() === null) {
+                con = connectionsRef.push("player1");
+                con.onDisconnect().remove();
+                userIam="player1"
+            } else if (Object.values(ss.val()).includes("player1")) {
+                con = connectionsRef.push("player2");
+                con.onDisconnect().remove();
+                userIam="player2"
             } else {
-                usersRef.set({
-                    player2: input,
-                    wins: 0,
-                    losses: 0,
-                })
-                $("#player2name").text(input);
+                con = connectionsRef.push("player1");
+                con.onDisconnect().remove();
+                userIam="player1"
             }
-        });
-        $("input").val("");
-    })
+            console.log(ss.val(), "data")
+            console.log(userIam)
+        })
+        // var con = connectionsRef.push("true");
+    }
+});
 
+$("#nameEntry").on("click", function (event) {
+    event.preventDefault();
+    var input = $("input").val().trim();
+    connectionsRef.on("value", function (snap) {
+        if (userIam = "player2") {
+            usersRef.push({
+                player2: input,
+                wins: 0,
+                losses: 0,
+            })
+            $("#player2name").text(input);
+        } else if (userIam = "player1") {
+            usersRef.push({
+                player1: input,
+                wins: 0,
+                losses: 0,
+            })
+            $("#player1name").text(input);
+        }
+    });
+    $("input").val("");
+})
 
 //    var clickCounter = 0;
 
